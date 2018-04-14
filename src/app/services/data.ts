f7cf4c4983b57a8545320960846a65f98814ca53
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2018-04-14 13:51:38 +0200

    Added badge to top charities.

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -9,7 +9,8 @@ export class DataService {
       website: 'https://www.againstmalaria.com',
       donationLink: 'https://www.againstmalaria.com/Donation.aspx',
       topCharity: true,
-      intervention: 'Marlaria Netze',
+      interventions: [ 'Marlaria Netze' ],
+      questionaireResults: [],
       slogan: 'Insecticide-treated nets to prevent malaria in sub-Saharan Africa.',
       shortDescription: 'Malaria is one of the leading killers of children in Africa. Insecticide-treated nets prevent malaria and are inexpensive — about $5 per net.'
     },
@@ -19,7 +20,8 @@ export class DataService {
       website: 'https://end.org/',
       donationLink: 'https://end.org/engage/invest/',
       topCharity: true,
-      intervention: 'Entwurmung',
+      intervention: [ 'Entwurmung' ],
+      questionaireResults: [],
       slogan: 'Supporting deworming programs in low-income countries.',
       shortDescription: 'The END Fund manages grants, raises funding and supports government programs for controlling and eliminating neglected tropical diseases, including deworming.'
     }
