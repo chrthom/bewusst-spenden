35aeee25e7f1e93bf009531b27903e656a08a7e8
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2018-05-13 10:00:33 +0200

    Changed data structure of organisations.

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -8,33 +8,74 @@ export class DataService {
       thumbnail: 'against_malaria_foundation',
       website: 'https://www.againstmalaria.com',
       donationLink: 'https://www.againstmalaria.com/Donation.aspx',
-      topCharity: true,
-      interventions: [ 'Malaria Netze' ],
+      category: [ 'Armut' ],
+      cause: [ 'Lebenssicherung', 'Gesundheit' ],
+      impactDirection: 'reactive',
+      regions: ['Afrika'],
       questionaireResults: [],
       slogan: 'Insecticide-treated nets to prevent malaria in sub-Saharan Africa.',
-      shortDescription: 'Malaria is one of the leading killers of children in Africa. Insecticide-treated nets prevent malaria and are inexpensive — about $5 per net.'
+      shortDescription: 'Malaria is one of the leading killers of children in Africa. Insecticide-treated nets prevent malaria and are inexpensive — about $5 per net.',
+      longDescription: 'Mehr Text... ungefähr 3-5 Sätze.',
+      impact1000: 'Was wird mit 1000€ erreicht? ...',
+      donationDeficit: 20000000,
+      evaluators: [
+        {
+          evaluator: 'GiveWell',
+          link: 'https://www.givewell.org/charities/AMF',
+          top: true
+        },
+        {
+          evaluator: 'The life you can save',
+          link: 'https://www.thelifeyoucansave.org/where-to-donate/against-malaria-foundation',
+          top: false
+        }
+      ]
     },
     {
       name: 'END Fund',
       thumbnail: 'end_fund',
       website: 'https://end.org/',
-      donationLink: 'https://end.org/engage/invest/',
-      topCharity: true,
-      interventions: [ 'Entwurmung' ],
+      donationLink: 'https://end.org/engage/invest',
+      category: [ 'Armut' ],
+      cause: [ 'Gesundheit' ],
+      impactDirection: 'reactive',
+      regions: ['Afrika'],
       questionaireResults: [],
       slogan: 'Supporting deworming programs in low-income countries.',
-      shortDescription: 'The END Fund manages grants, raises funding and supports government programs for controlling and eliminating neglected tropical diseases, including deworming.'
+      shortDescription: 'The END Fund manages grants, raises funding and supports government programs for controlling and eliminating neglected tropical diseases, including deworming.',
+      longDescription: 'Mehr Text... ungefähr 3-5 Sätze.',
+      impact1000: 'Was wird mit 1000€ erreicht? ...',
+      donationDeficit: 15000000,
+      evaluators: [
+        {
+          evaluator: 'GiveWell',
+          link: 'https://www.givewell.org/charities/AMF',
+          top: true
+        }
+      ]
     },
     {
       name: 'Future of Life Institute',
       thumbnail: 'future_of_life',
-      website: 'https://futureoflife.org/',
+      website: 'https://futureoflife.org',
       donationLink: 'https://futureoflife.org/get-involved/',
-      topCharity: false,
-      interventions: [ 'KI', 'Biotechnik', 'Nukleartechnologie', 'Klimaschutz' ],
+      category: [ 'Existenzrisiken' ],
+      cause: [ 'Künstliche Intelligenz', 'Biotechnologie', "Nukleartechnologie", "Klimaschutz" ],
+      impactDirection: 'proactive',
+      regions: ['Asien', "Europa", "USA"],
       questionaireResults: [],
       slogan: 'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      shortDescription: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+      shortDescription: 'orem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+      longDescription: 'Mehr Text... ungefähr 3-5 Sätze.',
+      impact1000: 'Was wird mit 1000€ erreicht? ...',
+      donationDeficit: null,
+      evaluators: [
+        {
+          evaluator: 'The life you can save',
+          link: 'https://www.thelifeyoucansave.org/where-to-donate/against-malaria-foundation',
+          top: false
+        }
+      ]
     }
   ];
 
