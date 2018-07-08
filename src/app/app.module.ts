10df084fdd38170bd2da7072a79866901f70b781
Christopher Thomsen <christopher@riskident.com>
2018-07-08 07:26:41 +0200

    First version with a working map (just dummy data yet).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { ChartModule, HIGHCHARTS_MODULES } from "angular-highcharts";
+import * as Highmaps from 'highcharts/modules/map.src';
+
 import { EADonationApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { QuestionnairePage } from '../pages/questionnaire/questionnaire';
 import { SearchPage } from "../pages/search/search";
-
-import { DataService } from "./services/data";
-import { ModalService } from "./services/modal";
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 import { OrganizationPage } from "../pages/organization/organization";
 import { PrivacyPage } from "../pages/privacy/privacy";
+
+import { DataService } from "./services/data";
+import { MapsService } from "./services/maps";
+import { ModalService } from "./services/modal";
 
 @NgModule({
   declarations: [
@@ -29,6 +34,7 @@ import { PrivacyPage } from "../pages/privacy/privacy";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ChartModule,
     IonicModule.forRoot(EADonationApp)
   ],
   bootstrap: [IonicApp],
@@ -43,10 +49,12 @@ import { PrivacyPage } from "../pages/privacy/privacy";
   ],
   providers: [
     DataService,
+    MapsService,
     ModalService,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [ Highmaps ] }
   ]
 })
 export class AppModule {}
--- src/pages/organization/organization.ts
+++ src/pages/organization/organization.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from "ionic-angular";
 import { Organization } from "../../app/model/organization";
+import { Chart, MapChart } from "angular-highcharts";
+import { MapsService } from "../../app/services/maps";
 
 @Component({
   templateUrl: 'organization.html'
@@ -8,7 +10,51 @@ import { Organization } from "../../app/model/organization";
 export class OrganizationPage {
   organization: Organization;
 
-  constructor(public viewCtrl: ViewController, public params: NavParams) {
+  chart = new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Line 1',
+        data: [1, 2, 3]
+      }
+    ]
+  });
+
+  worldmap = new MapChart({
+    chart: {
+      borderWidth: 1
+    },
+    title: {
+      text: 'Aktionsgebiete'
+    },
+    subtitle: {
+      text: 'Demo of drawing all areas in the map, only highlighting partial data'
+    },
+    legend: {
+      enabled: false
+    },
+    series: [{
+      name: 'Land',
+      data: [
+        ['is', 1],
+        ['no', 1],
+        ['se', 1],
+        ['dk', 1],
+        ['fi', 1]
+      ],
+      mapData: this.mapsService.worldmap
+    }]
+  });
+
+  constructor(public viewCtrl: ViewController, public params: NavParams, private mapsService: MapsService) {
     this.organization = this.params.get('o');
   }
 
