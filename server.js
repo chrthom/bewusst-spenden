a45eb68917d0982883d0b1aa7b132f57cec9cb79
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2019-08-05 13:59:36 +0000

    Merged in https_redirect (pull request #51)
    Https redirect via SSLify

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var enforce = require('express-sslify');
+var app = express();
 
-app.use(express.static('www'));
+//app.use(express.static('www'));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
